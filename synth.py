02f6ac5f113f4d9a58e1db49c7714c72744c72bd
Peter Lamut <inbox@peterlamut.com>
2020-01-31 14:13:44 +0000

    Reduce unit tests coverage threshold to 99%

--- synth.py
+++ synth.py
@@ -189,7 +189,7 @@ s.replace(
 # ----------------------------------------------------------------------------
 # Add templated files
 # ----------------------------------------------------------------------------
-templated_files = gcp.CommonTemplates().py_library(unit_cov_level=97, cov_level=100)
+templated_files = gcp.CommonTemplates().py_library(unit_cov_level=97, cov_level=99)
 s.move(templated_files)
 
 s.shell.run(["nox", "-s", "blacken"], hide_output=False)
