54092f9cf72531f1569f697aa02d0b9e43ab09e6
Tianzi Cai <tianzi@google.com>
2021-03-02 17:43:06 -0800

    samples: create, delete, get, list samples

---
+++ samples/snippets/schema_test.py
@@ -0,0 +1,204 @@
+#!/usr/bin/env python
+
+# Copyright 2021 Google LLC. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import sys
+import uuid
+
+from google.api_core.exceptions import NotFound
+from google.cloud import pubsub_v1
+import pytest
+
+import schema
+
+UUID = uuid.uuid4().hex
+PY_VERSION = f"{sys.version_info.major}.{sys.version_info.minor}"
+PROJECT_ID = os.environ["GOOGLE_CLOUD_PROJECT"]
+AVRO_TOPIC_ID = f"schema-test-avro-topic-{PY_VERSION}-{UUID}"
+PROTO_TOPIC_ID = f"schema-test-proto-topic-{PY_VERSION}-{UUID}"
+AVRO_SUBSCRIPTION_ID = f"schema-test-avro-subscription-{PY_VERSION}-{UUID}"
+PROTO_SUBSCRIPTION_ID = f"schema-test-proto-subscription-{PY_VERSION}-{UUID}"
+AVRO_SCHEMA_ID = f"schema-test-avro-schema-{PY_VERSION}-{UUID}"
+PROTO_SCHEMA_ID = f"schema-test-proto-schema-{PY_VERSION}-{UUID}"
+AVRO_FILE = "resources/us-states.avsc"
+PROTO_FILE = "resources/us-states.proto"
+
+
+@pytest.fixture(scope="module")
+def publisher_client():
+    yield pubsub_v1.PublisherClient()
+
+
+@pytest.fixture(scope="module")
+def avro_topic(publisher_client):
+    avro_topic_path = publisher_client.topic_path(PROJECT_ID, AVRO_TOPIC_ID)
+
+    try:
+        avro_topic = publisher_client.get_topic(request={"topic": avro_topic_path})
+    except NotFound:
+        avro_topic = publisher_client.create_topic(request={"name": avro_topic_path})
+
+    yield avro_topic.name
+
+    publisher_client.delete_topic(request={"topic": avro_topic.name})
+
+
+@pytest.fixture(scope="module")
+def proto_topic(publisher_client):
+    proto_topic_path = publisher_client.topic_path(PROJECT_ID, PROTO_TOPIC_ID)
+
+    try:
+        proto_topic = publisher_client.get_topic(request={"topic": proto_topic_path})
+    except NotFound:
+        proto_topic = publisher_client.create_topic(request={"name": proto_topic_path})
+
+    yield proto_topic.name
+
+    publisher_client.delete_topic(request={"topic": proto_topic.name})
+
+
+@pytest.fixture(scope="module")
+def subscriber_client():
+    subscriber_client = pubsub_v1.SubscriberClient()
+    yield subscriber_client
+    subscriber_client.close()
+
+
+@pytest.fixture(scope="module")
+def avro_subscription(subscriber_client, avro_topic):
+    avro_subscription_path = subscriber_client.subscription_path(
+        PROJECT_ID, AVRO_SUBSCRIPTION_ID
+    )
+
+    try:
+        avro_subscription = subscriber_client.get_subscription(
+            request={"subscription": avro_subscription_path}
+        )
+    except NotFound:
+        avro_subscription = subscriber_client.create_subscription(
+            request={"name": avro_subscription_path, "topic": avro_topic}
+        )
+
+    yield avro_subscription.name
+
+    subscriber_client.delete_subscription(
+        request={"subscription": avro_subscription.name}
+    )
+
+
+@pytest.fixture(scope="module")
+def proto_subscription(subscriber_client, proto_topic):
+    proto_subscription_path = subscriber_client.subscription_path(
+        PROJECT_ID, PROTO_SUBSCRIPTION_ID
+    )
+
+    try:
+        proto_subscription = subscriber_client.get_subscription(
+            request={"subscription": proto_subscription_path}
+        )
+    except NotFound:
+        proto_subscription = subscriber_client.create_subscription(
+            request={"name": proto_subscription_path, "topic": proto_topic}
+        )
+
+    yield proto_subscription.name
+
+    subscriber_client.delete_subscription(
+        request={"subscription": proto_subscription.name}
+    )
+
+
+@pytest.fixture(scope="module")
+def schema_client():
+    schema_client = pubsub_v1.SchemaServiceClient()
+    yield schema_client
+
+
+@pytest.fixture(scope="module")
+def avro_schema(schema_client):
+    avro_schema_path = schema_client.schema_path(PROJECT_ID, AVRO_SCHEMA_ID)
+
+    yield avro_schema_path
+
+    try:
+        schema_client.delete_schema(request={"name": avro_schema_path})
+    except NotFound:
+        pass
+
+
+@pytest.fixture(scope="module")
+def proto_schema(schema_client):
+    proto_schema_path = schema_client.schema_path(PROJECT_ID, PROTO_SCHEMA_ID)
+
+    yield proto_schema_path
+
+    try:
+        schema_client.delete_schema(request={"name": proto_schema_path})
+    except NotFound:
+        pass
+
+
+def _publish_messages(publisher_client, topic, message_num=5, **attrs):
+    for n in range(message_num):
+        data = f"message {n}".encode("utf-8")
+        publish_future = publisher_client.publish(topic, data, **attrs)
+        publish_future.result()
+
+
+def test_create_avro_schema(schema_client, avro_schema, capsys):
+    try:
+        schema_client.delete_schema(request={"name": avro_schema})
+    except NotFound:
+        pass
+
+    schema.create_avro_schema(PROJECT_ID, AVRO_SCHEMA_ID, AVRO_FILE)
+
+    out, _ = capsys.readouterr()
+    assert "Created a schema using an Avro schema file:" in out
+    assert f"{avro_schema}" in out
+
+
+def test_create_proto_schema(schema_client, proto_schema, capsys):
+    try:
+        schema_client.delete_schema(request={"name": proto_schema})
+    except NotFound:
+        pass
+
+    schema.create_proto_schema(PROJECT_ID, PROTO_SCHEMA_ID, PROTO_FILE)
+
+    out, _ = capsys.readouterr()
+    assert "Created a schema using a protobuf schema file:" in out
+    assert f"{proto_schema}" in out
+
+
+def test_get_schema(avro_schema, capsys):
+    schema.get_schema(PROJECT_ID, AVRO_SCHEMA_ID)
+    out, _ = capsys.readouterr()
+    assert "Got a schema" in out
+    assert f"{avro_schema}" in out
+
+
+def test_list_schemas(capsys):
+    schema.list_schemas(PROJECT_ID)
+    out, _ = capsys.readouterr()
+    assert "Listed schemas." in out
+
+
+def test_delete_schema(proto_schema, capsys):
+    schema.delete_schema(PROJECT_ID, PROTO_SCHEMA_ID)
+    out, _ = capsys.readouterr()
+    assert "Deleted a schema" in out
+    assert f"{proto_schema}" in out
