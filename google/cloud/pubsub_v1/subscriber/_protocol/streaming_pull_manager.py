7c023199cbe228c8fd649e82ec26426468b0d72a
Tianzi Cai <tianzi@google.com>
2021-03-16 14:21:44 -0700

    Update google/cloud/pubsub_v1/subscriber/_protocol/streaming_pull_manager.py

--- google/cloud/pubsub_v1/subscriber/_protocol/streaming_pull_manager.py
+++ google/cloud/pubsub_v1/subscriber/_protocol/streaming_pull_manager.py
@@ -108,7 +108,7 @@ class StreamingPullManager(object):
             control settings.
         use_legacy_flow_control (bool):
             If set to ``True``, it disables enforcing flow control settings on the
-            Cloud PubSub server. Defaults to ``False``.
+            Cloud Pub/Sub server. Defaults to ``False``.
             The less accurate method of enforcing flow control on the client side
             is still enabled in both cases.
         scheduler (~google.cloud.pubsub_v1.scheduler.Scheduler): The scheduler
