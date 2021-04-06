c8f589e4fcb2d252ba9752dad1e801039dd98c1e
Jim Fulton <jim@jimfulton.info>
2021-04-06 10:54:51 -0600

    Document the fixture.

--- tests/unit/pubsub_v1/conftest.py
+++ tests/unit/pubsub_v1/conftest.py
@@ -5,4 +5,8 @@ import pytest
 
 @pytest.fixture
 def creds():
+    """
+    Provide test creds to unit tests so that they can run without
+    GOOGLE_APPLICATION_CREDENTIALS set.
+    """
     yield mock.Mock(spec=google.auth.credentials.Credentials)
