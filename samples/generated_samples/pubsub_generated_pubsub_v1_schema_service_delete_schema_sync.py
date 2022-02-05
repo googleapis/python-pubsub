e0ed7f72f41911b84cfac1ac9a27c4feed6913b6
Anthonios Partheniou <partheniou@google.com>
2022-02-05 11:19:14 +0000

    use the correct package name in samples/generated_samples

--- samples/generated_samples/pubsub_generated_pubsub_v1_schema_service_delete_schema_sync.py
+++ samples/generated_samples/pubsub_generated_pubsub_v1_schema_service_delete_schema_sync.py
@@ -20,7 +20,7 @@
 # It may require modifications to work in your environment.
 
 # To install the latest published package dependency, execute the following:
-#   python3 -m pip install google-pubsub
+#   python3 -m pip install google-cloud-pubsub
 
 
 # [START pubsub_generated_pubsub_v1_SchemaService_DeleteSchema_sync]
