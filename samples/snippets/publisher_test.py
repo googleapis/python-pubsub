73594478b66305cff73e4460c879fcfe7c908be7
acocuzzo <annaco@google.com>
2022-04-22 17:54:58 -0400

    isort

--- samples/snippets/publisher_test.py
+++ samples/snippets/publisher_test.py
@@ -15,7 +15,7 @@
 import os
 import time
 import typing
-from typing import Any, Callable, cast, Iterator, TypeVar, Union
+from typing import Any, Callable, Iterator, TypeVar, Union, cast
 import uuid
 
 from _pytest.capture import CaptureFixture
