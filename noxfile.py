05ab21d095f941c105da046039553dbae3837ee6
acocuzzo <annaco@google.com>
2022-05-28 15:36:13 -0400

    chore(dependencies): add asyncio-mode=strict

--- noxfile.py
+++ noxfile.py
@@ -217,6 +217,7 @@ def default(session):
     # Run py.test against the unit tests.
     session.run(
         "py.test",
+        "--asyncio-mode=strict",
         "--quiet",
         f"--junitxml=unit_{session.python}_sponge_log.xml",
         "--cov=google/cloud",
