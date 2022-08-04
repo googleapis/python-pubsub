791fb37a908f8632ab9225ffebef014c14b89bae
Anna Cocuzzo <63511057+acocuzzo@users.noreply.github.com>
2022-08-04 13:27:26 -0400

    Update conftest.py

--- tests/unit/pubsub_v1/conftest.py
+++ tests/unit/pubsub_v1/conftest.py
@@ -13,7 +13,12 @@
 # limitations under the License.
 
 import google.auth.credentials
-from unittest import mock
+# try/except added for compatibility with python < 3.8
+try:
+    from unittest import mock
+    from unittest.mock import AsyncMock
+except ImportError:
+    import mock
 import pytest
 
 
