dc5aaf38d7fd32cb83141c03b0e34b134c6b4ed7
Anna Cocuzzo <63511057+acocuzzo@users.noreply.github.com>
2022-08-04 13:55:45 -0400

    Update test_heartbeater.py

--- tests/unit/pubsub_v1/subscriber/test_heartbeater.py
+++ tests/unit/pubsub_v1/subscriber/test_heartbeater.py
@@ -18,7 +18,12 @@ import threading
 from google.cloud.pubsub_v1.subscriber._protocol import heartbeater
 from google.cloud.pubsub_v1.subscriber._protocol import streaming_pull_manager
 
-from unittest import mock
+# try/except added for compatibility with python < 3.8
+try:
+    from unittest import mock
+    from unittest.mock import AsyncMock
+except ImportError:
+    import mock
 import pytest
 
 
