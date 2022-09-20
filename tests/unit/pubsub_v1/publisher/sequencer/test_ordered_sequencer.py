035ac5dd3f7c232489a15bc21eb0dcc61cf8aad8
Anna Cocuzzo <63511057+acocuzzo@users.noreply.github.com>
2022-09-20 18:13:36 -0400

    Update test_ordered_sequencer.py

--- tests/unit/pubsub_v1/publisher/sequencer/test_ordered_sequencer.py
+++ tests/unit/pubsub_v1/publisher/sequencer/test_ordered_sequencer.py
@@ -13,12 +13,14 @@
 # limitations under the License.
 
 import concurrent.futures as futures
+import sys
 
-# try/except added for compatibility with python < 3.8
-try:
-    from unittest import mock
-except ImportError:
+# special case python < 3.8
+if sys.version_info.major == 3 and sys.version_info.minor < 8:
     import mock
+else:
+    from unittest import mock
+
 import pytest
 
 from google.auth import credentials
