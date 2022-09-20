1c821517c81d2425872f188527ff19f3fc44e223
Anna Cocuzzo <63511057+acocuzzo@users.noreply.github.com>
2022-09-20 18:08:45 -0400

    Update test_streaming_pull_manager.py

--- tests/unit/pubsub_v1/subscriber/test_streaming_pull_manager.py
+++ tests/unit/pubsub_v1/subscriber/test_streaming_pull_manager.py
@@ -14,15 +14,16 @@
 
 import functools
 import logging
+import sys
 import threading
 import time
 import types as stdlib_types
 
-# try/except added for compatibility with python < 3.8
-try:
-    from unittest import mock
-except ImportError:
+# special case python < 3.8
+if sys.version_info.major == 3 and sys.version_info.minor < 8:
     import mock
+else:
+    from unittest import mock
 import pytest
 
 from google.api_core import bidi
