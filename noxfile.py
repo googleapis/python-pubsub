8d6bd446f02f6cc82448a76948de67be96ad5161
acocuzzo <annaco@google.com>
2023-01-10 12:59:06 -0500

    fix: pin mock version to 5.0.0 in noxfile for unit tests

--- noxfile.py
+++ noxfile.py
@@ -35,7 +35,7 @@ DEFAULT_PYTHON_VERSION = "3.8"
 
 UNIT_TEST_PYTHON_VERSIONS = ["3.7", "3.8", "3.9", "3.10", "3.11"]
 UNIT_TEST_STANDARD_DEPENDENCIES = [
-    "mock",
+    "mock==5.0.0",
     "asyncmock",
     "pytest",
     "pytest-cov",
