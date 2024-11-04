1ee5d36a92d3518914b798961ff698dbe537436e
ohmayr <omairn@google.com>
2024-11-04 22:53:46 +0000

    remove nox

--- owlbot.py
+++ owlbot.py
@@ -324,7 +324,7 @@ for library in s.get_staging_dirs(default_version):
     if count < 1:
         raise Exception(".coveragerc replacement failed.")
 
-    s.move([library], excludes=["**/gapic_version.py", "README.rst", "docs/**/*", "setup.py", "testing/constraints-3.7.txt", "testing/constraints-3.8.txt"])
+    s.move([library], excludes=["**/gapic_version.py", "README.rst", "docs/**/*", "setup.py", "testing/constraints-3.7.txt", "testing/constraints-3.8.txt", "noxfile.py"])
 s.remove_staging_dirs()
 
 # ----------------------------------------------------------------------------
